46e8409526f903434f0c70a2c7cdee844364dba3
Benjamin Chiong <bjchiong@gmail.com>
2020-08-02 22:48:35 +1000

    introduce material design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import Button from '@material-ui/core/Button'
+import Paper from '@material-ui/core/Paper'
+import { CircularProgress } from '@material-ui/core'
+import Fab from '@material-ui/core/Fab';
+import StopIcon from '@material-ui/icons/Stop';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import './App.css';
 
 function App() {
@@ -13,48 +19,53 @@ function App() {
 
 let timer;
 let seconds;
-let stage;
+let stage = 0;
+let minutes;
 class Timer extends React.Component {
   constructor(props) {
+
     super(props);
       this.state = {
-        time: 60 * 0.1,
+        time: 60 * 24,
         isPause: false,
-        phase: 1,
+        phase: 0,
       };
   }
 
   startTimer() {
     stage = this.state.phase;
+    minutes = getTimeLimit(this.state.phase);
     if (this.state.isPause) {
       seconds = this.state.time;
       timer = (function () { return; })();
     } else {
-      seconds = 60 * 0.1;
+      seconds = 60 * minutes;
     }
 
     if(!timer) {
       timer = setInterval(() => {
-        if (stage === 4) {
+        if (stage === 7) {
           stage = 0; // reset
         }
 
-        if (seconds <= 0) {
+        // break
+        if (seconds < 0) {
           this.setState({
             time: 0,
             phase: stage + 1,
           });
+          clearInterval(timer);
+          timer = (function () { return; })();
         }
 
         seconds = seconds - 1;
+
         this.setState({
           time: seconds,
           isPause: false,
         });
       }, 1000);
     }
-    // if condition then set to stage 2
-
   }
 
   stopTimer() {
@@ -78,14 +89,27 @@ class Timer extends React.Component {
   }
   // toggle button
     render() {
+      const phases = ['1', 'Rest', '2', 'Rest', '3', 'Rest', '4', 'End'];
       return (
       <div>
-      Phase
+<Paper elevation={1} />
+Phase
       <p>{this.state.phase}</p>
+      <p>{phases[this.state.phase]}</p>
+
+      <CircularProgress variant="static" value={calculatePercentage(this.state.time, 24 * 60)} >
       <p>{secondsToHms(this.state.time)}</p>
-      <button onClick={() => this.startTimer()}>Start timer</button>
-      <button onClick={() => this.stopTimer()}>Stop timer</button>
-      <button onClick={() => this.reset()}>Reset</button></div>
+      </CircularProgress>
+      <Fab onClick={() => this.startTimer()} color="primary" aria-label="play">
+        <PlayArrowIcon></PlayArrowIcon>
+      </Fab>
+      <Fab onClick={() => this.stopTimer() } color="primary" aria-label="play">
+        <StopIcon></StopIcon>
+      </Fab>
+      <Button onClick={() =>  this.reset()} variant="contained" color="primary">Reset</Button>
+<Paper />
+
+      </div>
 );
     }
 }
@@ -100,6 +124,18 @@ function secondsToHms(d) {
   var mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
   var sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
   return hDisplay + mDisplay + sDisplay; 
+}
+
+function getTimeLimit(stage) {
+  if (stage % 2 === 0) {
+    return 24;
+  } else {
+    return 5;
+  }
+}
+
+function calculatePercentage(duration, limit) {
+  return (duration / limit) * 100; 
 }
 
 export default App;
